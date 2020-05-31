07784dcd6d50470956dd9e3376c4c28e463d0724
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-05-31 15:18:00 -0500

    add tarea a JSON

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
-
 //console.log(argv);
+
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0];
 
 switch (comando) {
 
     case 'crear':
-        console.log('Crear Tarea');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
 
     case 'listar':
