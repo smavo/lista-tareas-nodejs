d880f7fe32a59c850424c652d505023aed1b9c8b
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-05-31 14:46:54 -0500

    add comandos basicos de la app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // const argv = require('yargs').argv;
-const argv = require('yargs').argv;
+const argv = require('./config/yargs').argv;
 
-console.log(argv);
+//console.log(argv);
 
 let comando = argv._[0];
 
